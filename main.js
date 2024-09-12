4662cee068db0730dc0dfded1005b40fa710b073
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-12 21:58:34 +0500

    javohirniki

---
+++ main.js
@@ -0,0 +1,101 @@
+let num = parseInt("123");
+
+console.log(num);
+// sedcond homework
+
+let number = parseFloat("3.14");
+
+console.log(number);
+
+// 3
+
+let know = "0x1A";
+let numbe = parseInt(know, 16);
+
+console.log(numbe);
+
+//4
+const numberNone = "1010";
+const numb = parseInt(numberNone, 2);
+
+console.log(numb);
+
+// 5
+let numberSecond;
+
+console.log(Number.MAX_VALUE);
+
+//6
+
+let numberFirst = 434;
+
+console.log(Number.isInteger(numberFirst));
+
+//7
+let numberFour = 2 ^ 53;
+
+console.log(Number.isSafeInteger(numberFour));
+
+//8
+
+let noneNumber = " 345 ";
+let numberFive = parseInt(noneNumber, 10);
+
+console.log(numberFive);
+
+//9
+let result = parseInt("abc123");
+
+console.log(result);
+
+//10
+
+//11
+let numberSix;
+
+console.log(Number.MIN_VALUE);
+
+//12
+
+let numberSeven = parseFloat("56.78");
+
+console.log(numberSeven);
+
+//13
+let numberNine = Math.round(7.5);
+
+console.log(numberNine);
+
+//14
+let numberValue = parseFloat("9.81abc");
+
+console.log(numberValue);
+
+//15
+let water = parseInt("abc");
+
+console.log(water);
+
+//16
+
+let milk = parseInt("0123");
+
+console.log(milk);
+
+//17
+
+let pill;
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+//18
+
+//19
+let mill;
+
+console.log(Number.MIN_SAFE_INTEGER);
+
+//20
+let bigValue = Math.floor(78.99);
+
+console.log(bigValue);
